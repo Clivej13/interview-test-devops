52a59ddfee6831123e5f1dfc6c4fb0680a1b4118
James Clive <j.clive@hotmail.co.uk>
2020-10-07 11:49:05 +0100

    simple web server cluster

--- 03_infrastructure_test/terraformScripts/main.tf
+++ 03_infrastructure_test/terraformScripts/main.tf
@@ -2,11 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
+data "aws_availability_zones" "all" {}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -18,23 +14,73 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [
-	aws_security_group.instance.id,
-	]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the web server"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones = data.aws_availability_zones.all.names
+	
+  min_size = 2
+  max_size = 10
+
+  load_balancers    = [aws_elb.example.name]
+  health_check_type = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = [aws_security_group.elb.id]
+  availability_zones = data.aws_availability_zones.all.names
+  
+  health_check {
+    target              = "HTTP:${var.server_port}/"
+    interval            = 30
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+  
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
